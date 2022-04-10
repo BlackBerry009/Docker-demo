7f1ff6ba4ac415ad38e86333d3cc2b0f4fabbde9
Artist M <baymax97@163.com>
2022-04-10 17:43:10 +0800

    Finish configuration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-This is a docker practice demo
+This is a docker practice demo which is using docker-compose configuration
 
-use different configuration to complete it 
+
+Tip:
+    1. React project which is using docker, must start with interactive and terminal mode,otherwise can not start successfully, if using docker-compose yaml configuration, must set  `stdin_open: true`   `tty: true` options to start -it mode .
+
+    
--- backend/app.js
+++ backend/app.js
@@ -82,9 +82,9 @@ app.delete('/goals/:id', async (req, res) => {
     res.status(500).json({ message: 'Failed to delete goal.' });
   }
 });
-
 mongoose.connect(
-  'mongodb://localhost:27017/course-goals',
+  // https://www.mongodb.com/docs/manual/reference/connection-string/
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -94,7 +94,7 @@ mongoose.connect(
       console.error('FAILED TO CONNECT TO MONGODB');
       console.error(err);
     } else {
-      console.log('CONNECTED TO MONGODB');
+      console.log('CONNECTED TO MONGODB!!');
       app.listen(80);
     }
   }
